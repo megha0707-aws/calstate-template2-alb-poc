4449158ac01c43990a25cba0426b0dd7884418b9
Megha0707 <meghana.srinivas0707@gmail.com>
2026-05-27 17:52:36 +0530

    Add subscription_id variable

--- calstate-template2/variables.tf
+++ calstate-template2/variables.tf
@@ -1,4 +1,19 @@
-variable "resource_group_name" {}
-variable "vnet_name" {}
-variable "aks_cluster_name" {}
-variable "name_prefix" {}
+variable "resource_group_name" {
+  type = string
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "aks_cluster_name" {
+  type = string
+}
+
+variable "name_prefix" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
