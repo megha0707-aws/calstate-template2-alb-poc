55dc8c7c02136af4c7e40b4e859aedb4ce2f0fb7
Megha0707 <meghana.srinivas0707@gmail.com>
2026-05-27 17:22:21 +0530

    Fix backend auth for ALB deployment

--- calstate-template2/backend.tf
+++ calstate-template2/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "Grouper"
-    storage_account_name = "groupertfstate"
+    storage_account_name = "groupertfstatepoc01"
     container_name       = "terraform"
-    key                  = "dcs-apps-alb-poc.tfstate"
+    key                  = "dcs-apps-alb.tfstate"
   }
 }
