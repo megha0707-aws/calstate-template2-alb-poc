aee588b4602a4b607270b031931a417107e1c8c7
Megha0707 <meghana.srinivas0707@gmail.com>
2026-05-27 17:48:29 +0530

    Fix provider config

--- calstate-template2/provider.tf
+++ calstate-template2/provider.tf
@@ -9,14 +9,40 @@ terraform {
       source  = "azure/azapi"
       version = "~> 2.0"
     }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.16"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.32"
+    }
   }
 }
 
 provider "azurerm" {
   features {}
-  use_cli = false
+  subscription_id = var.subscription_id
 }
 
 provider "azapi" {
-  use_cli = false
+  subscription_id = var.subscription_id
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.azurerm_kubernetes_cluster.grouper.kube_config[0].host
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.grouper.kube_config[0].client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.grouper.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.grouper.kube_config[0].cluster_ca_certificate)
+  }
+}
+
+provider "kubernetes" {
+  host                   = data.azurerm_kubernetes_cluster.grouper.kube_config[0].host
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.grouper.kube_config[0].client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.grouper.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.grouper.kube_config[0].cluster_ca_certificate)
 }
